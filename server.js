c8cda94caa2d9c1b64c0c5cc331c93f08bd56212
Girishsonawane <girishjan2020@gmail.com>
2022-10-29 19:34:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Rabbit and Tortoise Technology Solutions pvt ltd Pune!!');
+  res.send('Welcome to Rabbit and Tortoise Technology Solutions pvt ltd kharadi!!');
 });
 
 app.listen(PORT, HOST);
